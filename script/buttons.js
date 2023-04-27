2731e5fb08f96427e585a8c52bc5b343b01bb819
Cristiano Reis <117485646+Akumanoir@users.noreply.github.com>
2023-04-27 17:33:31 -0300

    update

--- script/buttons.js
+++ script/buttons.js
@@ -1,57 +1,56 @@
 let loadMoreBnt = document.querySelector('#load-more')
-let currentItem = 9;
+let currentItem = 0;
+let boxes = [...document.querySelectorAll('button.card-monster')]
+
+window.onload = function(){
+    for (var i = 0; i < 9 ; i++) {
+        boxes[i].style.display = 'inline-block'
+    }
+}
 
 loadMoreBnt.onclick = () => {
-    let boxes = [...document.querySelectorAll('button.card-monster')]
-    for (var i = currentItem; i < currentItem + 9; i++) {
+    for (var i = currentItem; i < currentItem + 9 ; i++) {
+        
+        if(boxes[i] == undefined) {
+            continue
+        }
         boxes[i].style.display = 'inline-block'
     }
     currentItem += 9;
 
-    //fazer funcionar
     if(currentItem >= boxes.length) {
-        loadMoreBnt.style.display = 'none';
+        loadMoreBnt.style.display = 'none';   
     }
 }
-/*
-function all() {
-    alert('teste')
+
+function tudo() {
+    loadMoreBnt.style.display = 'block'
+    for (var i = 0; i < boxes.length; i++) {
+        boxes[i].style.display = 'none'
+    }
+    currentItem = 0
+    for (var i = 0; i < currentItem + 9 ; i++) {
+        if(boxes[i] == undefined) {
+            continue
+        }
+        boxes[i].style.display = 'inline-block'
+    }
+    currentItem += 9;
+
+    if(currentItem >= boxes.length) {
+        loadMoreBnt.style.display = 'none';   
+    }
 }
 
 function fangedWyvern() {
-    document.querySelector('body').classList.add('showfanged')  
+    for (var i = 0; i < boxes.length ; i++) {
     
+        if(boxes[i].className.includes('fanged-wyverns')) {
+            boxes[i].style.display = 'inline-block'  
+        } else {
+            boxes[i].style.display = 'none'
+        }
+    }
+    loadMoreBnt.style.display = 'none'
 }
 
-function fangedBeast() {
-    alert('teste')
-}
-
-function birdWyvern() {
-    alert('teste')
-}
-
-function bruteWyvern() {
-    alert('teste')
-}
-
-function flyingWyvern() {
-    alert('teste')
-}
-
-function piscineWyvern() {
-    alert('teste')
-}
-
-function elderDragon() {
-    alert('teste')
-}
-
-function relicts() {
-    alert('teste')
-}
-
-function smallMonster() {
-    alert('teste')
-}
-*/
